d1c7b82e98e7f937b905863780d6a52939a88fa7
R6145 <fahadwisam18@gmail.com>
2024-01-12 15:46:33 +0400

    latest

--- Bonus/pipex.c
+++ Bonus/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fmaqdasi <fmaqdasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 12:41:46 by fmaqdasi          #+#    #+#             */
-/*   Updated: 2024/01/11 20:08:29 by fmaqdasi         ###   ########.fr       */
+/*   Updated: 2024/01/12 14:52:49 by fmaqdasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,8 @@ void	middle(char **argv, char **envp, int **pipe_fd, int i)
 	if (pid == 0)
 	{
 		middle_child(argv, envp, pipe_fd, i);
-		waitpid(pid - 1, NULL, 0);
+		wait(NULL);
 	}
-	// waitpid(pid, NULL, 0);
 }
 
 void	parent(char **argv, char **envp, int **pipe_fd, int i)
@@ -75,7 +74,7 @@ void	parent(char **argv, char **envp, int **pipe_fd, int i)
 		ft_putstr_fd(strerror(errno), 2);
 		write(2, "\n", 1);
 		return (close_pipe(pipe_fd, argc_calc(argv)), free_pipe(pipe_fd),
-			exit(3));
+			exit(1));
 	}
 	if (dup2(fd, STDOUT_FILENO) == -1 || dup2(pipe_fd[i - 3][0],
 			STDIN_FILENO) == -1)
@@ -96,16 +95,14 @@ int	main(int argc, char **argv, char **envp)
 	pid_t	pid;
 	int		**fd;
 	int		i;
-	int		exitcode;
 
 	if (argc < 5)
-		return (exit(10), 0);
+		return (ft_putstr_fd("Wrong number of arguements\n", 2), exit(10), 0);
 	i = 3;
 	fd = fd_create(argc);
 	pid = forking(fd, argc);
 	if (pid == 0)
 		child(argv, envp, fd, i - 1);
-	waitpid(pid, &exitcode, 0);
 	while (i < argc - 2)
 	{
 		middle(argv, envp, fd, i);
@@ -115,8 +112,7 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
-
-//fix 5 arg error
-//handle exit codes
-//here_doc
-//make sure "cat /dev/random" "head"
+// fix 5 arg error yep
+// handle exit codes i think
+// here_doc
+// make sure "cat /dev/random" "head" yep
